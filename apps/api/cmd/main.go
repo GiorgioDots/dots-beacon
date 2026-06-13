602d376dda82c33c1f92273b4afbdef5c51bee41
Giorgio Dodesini <giorgiodotsdev@gmail.com>
2026-06-14 00:35:09 +0200

    testing db connection

--- apps/api/cmd/main.go
+++ apps/api/cmd/main.go
@@ -1,20 +1,31 @@
 package main
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/caarlos0/env/v11"
 	"github.com/giorgio-dots/dots-beacon-api/config"
+	"github.com/giorgio-dots/dots-beacon-internal/database/db"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func main() {
-	fmt.Println("hello worldds")
-
 	cfg, err := env.ParseAs[config.AppConfig]()
 	if err != nil {
 		fmt.Printf("An error occured while parsing environment variables %v\n", err)
 		return
 	}
 
-	fmt.Printf("%s\n", cfg.DatabaseUrl)
+	pool, err := pgxpool.New(context.Background(), cfg.DatabaseUrl)
+
+	q := db.New(pool)
+
+	plants, err := q.GetPlants(context.Background())
+	if err != nil {
+		fmt.Printf("An error occured while retrieving the plants %v\n", err)
+		return
+	}
+
+	fmt.Printf("Plants: %v\n", plants)
 }
