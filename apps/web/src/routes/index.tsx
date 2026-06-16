acde8d585f777db301d99590d98fd72ef338e236
Giorgio Dodesini <giorgiodotsdev@gmail.com>
2026-06-16 21:43:13 +0200

    testing keycloak js

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,14 +1,30 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { Button } from "#/components/ui/button";
+import keycloak from "#/lib/auth/keycloak";
+import { createFileRoute } from "@tanstack/react-router";
+import { useEffect } from "react";
 
-export const Route = createFileRoute('/')({ component: Home })
+export const Route = createFileRoute("/")({ component: Home });
 
 function Home() {
+  useEffect(() => {
+    async function test() {
+      try {
+        // const authenticated = await keycloak.init();
+        // if (authenticated) {
+        //   console.log("User is authenticated");
+        // } else {
+        //   console.log("User is not authenticated");
+        // }
+        console.log(keycloak.token);
+      } catch (error) {
+        console.error("Failed to initialize adapter:", error);
+      }
+    }
+    test();
+  });
   return (
-    <div className="p-8">
-      <h1 className="text-4xl font-bold">Welcome to TanStack Start</h1>
-      <p className="mt-4 text-lg">
-        Edit <code>src/routes/index.tsx</code> to get started.
-      </p>
-    </div>
-  )
+    <>
+      <Button onClick={() => keycloak.logout()}>logout</Button>
+    </>
+  );
 }
